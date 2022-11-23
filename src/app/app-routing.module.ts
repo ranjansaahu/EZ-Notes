3c90c1f3573811e007b69390cffa33b65e0822fd
Ranjan Kumar <nburanjan@gmail.com>
2022-11-23 16:30:07 +0530

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NotesComponent } from './notes/notes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NotesComponent },
+  { path: 'edit_note/:id', component: EditTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
